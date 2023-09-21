a4d7334c2a3922a0cf02b53bb65bff87757b1665
byorund <144137564+byorund@users.noreply.github.com>
2023-09-21 12:24:56 +0200

    Add files via upload

---
+++ Project 2 - Optimisation/test_helper_functions.py
@@ -0,0 +1,19 @@
+import unittest
+import numpy as np
+
+from helper_methods import approximate_hessian
+from optimization_problem import OptimizationProblem
+
+class TestNumericalHessian(unittest.TestCase):
+	
+	# f(x) = x^2 + y^2 --> hessian should be [[2, 0], [0, 2]]
+	def test_basic_functionality(self):
+		opt_problem = OptimizationProblem(lambda x: x[0]**2 + x[1]**2)
+		
+		hessian = approximate_hessian(opt_problem.get_function(), opt_problem.get_gradient(), [1, 1])
+		expected = np.array([[2, 0], [0, 2]])
+		np.testing.assert_array_almost_equal(hessian, expected, decimal=2)
+		
+# Run all unit tests
+if __name__ == "__main__":
+	unittest.main()
