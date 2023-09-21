89d6d6496048932085fe14481591d34118173525
byorund <144137564+byorund@users.noreply.github.com>
2023-09-21 12:32:21 +0200

    Update test_helper_functions.py

--- Project 2 - Optimisation/test_helper_functions.py
+++ Project 2 - Optimisation/test_helper_functions.py
@@ -4,7 +4,7 @@ import numpy as np
 from helper_methods import approximate_hessian
 from optimization_problem import OptimizationProblem
 
-class TestNumericalHessian(unittest.TestCase):
+class TestApproximateHessian(unittest.TestCase):
 	
 	# f(x) = x^2 + y^2 --> hessian should be [[2, 0], [0, 2]]
 	def test_basic_functionality(self):
@@ -13,6 +13,12 @@ class TestNumericalHessian(unittest.TestCase):
 		hessian = approximate_hessian(opt_problem.get_function(), opt_problem.get_gradient(), [1, 1])
 		expected = np.array([[2, 0], [0, 2]])
 		np.testing.assert_array_almost_equal(hessian, expected, decimal=2)
+		
+	def test_output_size(self):
+		opt_problem = OptimizationProblem(lambda x: x[0]**2 + x[1]**2)
+		
+		hessian = approximate_hessian(opt_problem.get_function(), opt_problem.get_gradient(), [1, 1])
+		self.assertEqual(hessian.shape, (2, 2))
 		
 # Run all unit tests
 if __name__ == "__main__":
