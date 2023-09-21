c39d48771a46d2fc7dc7071467a48130028e03ac
byorund <144137564+byorund@users.noreply.github.com>
2023-09-21 08:57:30 +0200

    Update helper_methods.py

--- Project 2 - Optimisation/helper_methods.py
+++ Project 2 - Optimisation/helper_methods.py
@@ -1,43 +1,36 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 #import tensorflow as tf
 import importlib.util
 
-
-
 # Default numerical method if gradient is not specified
-def numerical_gradient(self, x, h=1e-5):
-    return (self.evaluate(x + h) - self.evaluate(x - h)) / (2.0 * h)
+def numerical_gradient(evaluate_func, x, h=1e-5):
+    return (evaluate_func(x + h) - evaluate_func(x - h)) / (2.0 * h)
 
-def numerical_hessian(self, x, h=1e-5):
-    
-    dim=len(self.inspect.signature(self.objective_func))
+# added n since no access to self object anymore
+def numerical_hessian(evaluate_func, x, n, h=1e-5):
+    dim = n
     hessian=np.zeros((dim,dim))
     identity_matrix=np.diag(np.ones(dim))
 
     for i in range(0,dim-1):
         for j in range(0,dim-1):
         
-            hessian[i][j]=(self.evaluate(x+h*identity_matrix[i])+self.evaluate(x+h*identity_matrix[j])-2*self.evaluate(x))/h**2
+            hessian[i][j]=(evaluate_func(x+h*identity_matrix[i])+evaluate_func(x+h*identity_matrix[j])-2*evaluate_func(x))/h**2
 
     return hessian
 
-
-def inv_numerical_hessian(self,x,h=1e-5):
-
-    return np.linalg.inv(numerical_hessian(self,x,h))
-
-
+def inv_numerical_hessian(evaluate_func, x, n, h=1e-5):
+    return np.linalg.inv(numerical_hessian(evaluate_func, x, n, h))
 
 # Plot function with specified x_range and number of points
-def plot_objective_2D(self, range_x = None, num_points=100):
+def plot_objective_2D(evaluate_func, range_x = None, num_points=100):
     if range_x is None:
         print("Range for x is not specified")
         return
     
     x_values = np.linspace(range_x[0], range_x[1], num_points)
-    y_values = [self.evaluate(x) for x in x_values]
+    y_values = [evaluate_func(x) for x in x_values]
     
     plt.plot(x_values, y_values)
     plt.xlabel("x")
@@ -48,5 +41,3 @@ def plot_objective_2D(self, range_x = None, num_points=100):
 # 3D visualisation?
 def plot_objective_3D(self):
     return
-    
-
