1c43a3ce2c728780069d05a95fd1320039ddeb9c
byorund <144137564+byorund@users.noreply.github.com>
2023-09-22 12:40:55 +0200

    Update optimization_method.py

--- Project 2 - Optimisation/optimization_method.py
+++ Project 2 - Optimisation/optimization_method.py
@@ -1,6 +1,10 @@
 from helper_methods import numerical_hessian
 from helper_methods import numerical_gradient
 from helper_methods import inv_numerical_hessian
+from helper_methods import approximate_hessian
+
+from scipy.optimize import minimize_scalar
+from tqdm import tqdm
 
 import inspect
 import numpy as np
@@ -58,8 +62,9 @@ class ClassicalNewtonMethod(OptimizationMethod):
 		return point[-1]
 
 class NewtonExactLineSearch(OptimizationMethod):
-	def __init__(self, opt_problem, h=1e-5, tolerance=1e-5, max_iterations=1000):
+	def __init__(self, opt_problem, n, h=1e-12, tolerance=1e-5, max_iterations=1000):
 		super().__init__(opt_problem)
+		self.n = n
 		self.h = h
 		self.tolerance = tolerance
 		self.max_iterations = max_iterations
@@ -68,48 +73,62 @@ class NewtonExactLineSearch(OptimizationMethod):
 		self.validate_params()
 		
 	def exact_line_search(self, x, direction):
-		# To do
-		return x
+		# define g(alpha) = f(x + alpha * direction)
+		def g(alpha):
+			return self.opt_problem.evaluate(x + alpha * direction)
+		
+		# Cheat and use scipy to minimize the function
+		result = minimize_scalar(g, bracket=[0, 1])
+		
+		if result.success:
+			return result.x # return best alpha
+		else:
+			raise ValueError("Line search failed: " + result.message)
 		
 	def solve(self):	
-		n = self.opt_problem.get_num_of_parameters()
+		evaluate_func = self.opt_problem.get_evaluate()
+		gradient_fuc = self.opt_problem.get_gradient()
 		
-		# Start with zeros
-		x = np.zeros(n)
+		# Start with zeroscan
+		x = np.random.random(self.n)
 		iteration = 0
 		
-		while True:
-			current_value = self.opt_problem.evaluate(x)
-			current_gradient = self.opt_problem.gradient(x)
+		with tqdm(total=self.max_iterations, desc="Optimizing", unit="iteration") as pbar:
+			while True:
+				current_value = self.opt_problem.evaluate(x)
+				current_gradient = self.opt_problem.gradient(x)
 			
-			# Stopping criteria
-			if np.linalg.norm(current_gradient) < self.tolerance:
-				print(f"Converged in {iteration} iterations")
-				return x
+				# Stopping criteria
+				if np.linalg.norm(current_gradient) < self.tolerance:
+					print(f"Converged in {iteration} iterations")
+					return x
 			
-			if iteration >= self.max_iterations:
-				print("Maximum iterations reached.")
-				return x
+				if iteration >= self.max_iterations:
+					print("Maximum iterations reached.")
+					return x
 			
-			# Compute approximate Hessian
-			H = numerical_hessian(self.opt_problem.get_evaluate(), x, n)
+				# Compute approximate Hessian
+				H = approximate_hessian(evaluate_func, gradient_fuc, x, self.n)
 		
-			# Make symmetric if necessary
-			G = 0.5 * (H + H.T)
+				# Make symmetric if necessary
+				G = 0.5 * (H + H.T)
 			
-			# Newton's direction
-			direction = -np.dot(np.linalg.inv(G), current_gradient)
+				# Newton's direction
+				direction = -np.linalg.solve(G, current_gradient)
 		
-			# Do exact line search to find the optimal step size
-			alpha = self.exact_line_search(x, direction)
+				# Do exact line search to find the optimal step size
+				alpha = self.exact_line_search(x, direction)
 			
-			# Update the current point
-			x += alpha * direction
+				# Update the current point
+				x += alpha * direction
 			
-			iteration += 1
+				iteration += 1
+			
+				# Update the progress bar
+				pbar.update(1)
 		
-		# Minimum found
-		return x
+			# Minimum found
+			return x
 			
 	# Check that parameters are greater than zero
 	def validate_params(self):
