8d6a4a969ab2184de2f5a3fd2dbcb6db09b1be66
byorund <144137564+byorund@users.noreply.github.com>
2023-09-22 12:57:11 +0200

    Update test_newton_exact_search.py

--- Project 2 - Optimisation/test_newton_exact_search.py
+++ Project 2 - Optimisation/test_newton_exact_search.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-import inspect
+import matplotlib.pyplot as plt
 
 from optimization_method import NewtonExactLineSearch, OptimizationMethod
 from optimization_problem import OptimizationProblem
@@ -16,20 +16,34 @@ class TestNewtonExactLineSearch(unittest.TestCase):
 		with self.assertRaises(ValueError):
 			opt_method = NewtonExactLineSearch(self.mock_problem, n=2, h=-1, tolerance=-1)
 			
-	def test_rosen_brock(self):
-		print("\nTesting Rosenbrock")
-		solver = NewtonExactLineSearch(self.rosen_brock, n=2)
-		expected_solution = np.array([1.0, 1.0])
-		
-		np.testing.assert_allclose(solver.solve(), expected_solution, atol=1e-1)
-			
 	def test_max_iterations_stopping_criteria(self):
 		opt_method = NewtonExactLineSearch(self.mock_problem, n=2, max_iterations=1)
 		result = opt_method.solve()
 		
 		# For a mock quadratic function and only 1 iteration, the result should be [0, 0]
 		np.testing.assert_array_almost_equal(result, np.array([0, 0]))
+			
+	def test_rosen_brock(self):
+		print("\nTesting Rosenbrock")
+		solver = NewtonExactLineSearch(self.rosen_brock, n=2)
+		expected_solution = np.array([1.0, 1.0])
+
+		# Check solution
+		np.testing.assert_allclose(solver.solve(), expected_solution, atol=1e-1)
+		
+		path = np.array(solver.path)
+		# Create a grid for the contour plot
+		X, Y = np.meshgrid(np.linspace(-2, 2, 400), np.linspace(-1, 3, 400))
+		Z = self.rosen_brock.evaluate(np.array([X, Y]))
 	
+		# Create the contour plot
+		contour = plt.contour(X, Y, Z, levels=np.logspace(0, 5, 35), cmap='jet')
+		plt.plot(path[:, 0], path[:, 1], 'k-', linewidth=2)  # plot the optimization path
+		plt.plot(path[:, 0], path[:, 1], 'ro')  # mark the points along the path with red dots
+	
+		# Save the plot to a file
+		plt.savefig('contour_plot.png')
+		
 # Run all unit tests
 if __name__ == "__main__":
 	unittest.main()
