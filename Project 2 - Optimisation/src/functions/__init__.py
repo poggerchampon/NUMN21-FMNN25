6cd032d5b7853ebb8dfed823134b7ed6dd5c8a58
byorund <144137564+byorund@users.noreply.github.com>
2023-09-22 17:06:30 +0200

    Update __init__.py

---
+++ Project 2 - Optimisation/src/functions/__init__.py
@@ -0,0 +1,6 @@
+from .helper_methods import numerical_gradient
+from .helper_methods import numerical_hessian
+from .helper_methods import inv_numerical_hessian
+from .helper_methods import approximate_hessian
+
+__all__ = ['numerical_gradient', 'numerical_hessian', 'inv_numerical_hessian', 'approximate_hessian']
