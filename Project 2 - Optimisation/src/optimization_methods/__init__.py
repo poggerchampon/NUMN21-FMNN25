76b59dbba5a67c78ab3d1bab785276006aa18bf8
byorund <144137564+byorund@users.noreply.github.com>
2023-09-23 08:38:38 +0200

    Update __init__.py

--- Project 2 - Optimisation/src/optimization_methods/__init__.py
+++ Project 2 - Optimisation/src/optimization_methods/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 from .optimization_method import OptimizationMethod
 from .classical_newton_method import ClassicalNewtonMethod
-from .newton_exact_line_search import NewtonExactLineSearch
+from .newton_line_search import NewtonLineSearch
 
-__all__ = ['OptimizationMethod', 'ClassicalNewtonMethod', 'NewtonExactLineSearch']
+__all__ = ['OptimizationMethod', 'ClassicalNewtonMethod', 'NewtonLineSearch']
