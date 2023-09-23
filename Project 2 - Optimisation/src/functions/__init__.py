2165293f1096f3a73737290cd50ab8ea382b3db2
byorund <144137564+byorund@users.noreply.github.com>
2023-09-23 12:20:21 +0200

    Update __init__.py

--- Project 2 - Optimisation/src/functions/__init__.py
+++ Project 2 - Optimisation/src/functions/__init__.py
@@ -2,5 +2,6 @@ from .helper_methods import numerical_gradient
 from .helper_methods import numerical_hessian
 from .helper_methods import inv_numerical_hessian
 from .helper_methods import approximate_hessian
+from .save_rosenbrock_plot import save_rosenbrock_plot
 
-__all__ = ['numerical_gradient', 'numerical_hessian', 'inv_numerical_hessian', 'approximate_hessian']
+__all__ = ['numerical_gradient', 'numerical_hessian', 'inv_numerical_hessian', 'approximate_hessian', 'save_rosenbrock_plot']
