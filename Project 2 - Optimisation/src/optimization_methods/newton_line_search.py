f9518a319c27b5195b3eb670542d3f467f36d645
byorund <144137564+byorund@users.noreply.github.com>
2023-09-23 09:14:38 +0200

    Update newton_line_search.py

--- Project 2 - Optimisation/src/optimization_methods/newton_line_search.py
+++ Project 2 - Optimisation/src/optimization_methods/newton_line_search.py
@@ -9,12 +9,13 @@ import numpy as np
 # Can be specified to use either the exact line search
 # or the inexact line search using Armijo rule
 class NewtonLineSearch(OptimizationMethod):
-	def __init__(self, opt_problem, n, h=1e-12, tolerance=1e-5, max_iterations=1000, exact=True):
+	def __init__(self, opt_problem, n, h=1e-12, tolerance=1e-5, max_iterations=1000, initial_guess=None, exact=True):
 		super().__init__(opt_problem)
 		self.n = n
 		self.h = h
 		self.tolerance = tolerance
 		self.max_iterations = max_iterations
+		self.initial_guess = initial_guess
 		self.exact = exact
 		self.path = [] # Initiate an empty list to store the optimisation path
 		
@@ -34,9 +35,9 @@ class NewtonLineSearch(OptimizationMethod):
 		else:
 			raise ValueError("Line search failed: " + result.message)
 			
-	def inexact_line_search(self, x, direction, sigma=1e-4):
+	def inexact_line_search(self, x, direction, sigma=1e-4, max_backtracks=10):
 		alpha = 1
-		while True:
+		for _ in range(max_backtracks):
 			# Compute Armijo condition
 			f_new_x = self.opt_problem.evaluate(x + alpha * direction)
 			f_x = self.opt_problem.evaluate(x)
@@ -52,8 +53,8 @@ class NewtonLineSearch(OptimizationMethod):
 		evaluate_func = self.opt_problem.get_evaluate()
 		gradient_fuc = self.opt_problem.get_gradient()
 		
-		# Start with zeros
-		x = np.random.random(self.n)
+		# Start with zeros if no initial guess is specified
+		x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
 		iteration = 0
 		
 		# Use tqdm for neat progress bar
@@ -92,5 +93,9 @@ class NewtonLineSearch(OptimizationMethod):
 		
 	# Check that parameters are greater than zero
 	def validate_params(self):
-		if self.h <= 0 or self.tolerance <= 0:
-			raise ValueError("Paramaters should be greater than zero")
+		# Check that parameters are greater than zero
+		if self.h <= 0 or self.tolerance <= 0 or self.max_iterations <= 0 or self.n <= 0:
+			raise ValueError("Parameters should be greater than zero")
+		
+		if self.initial_guess is not None and len(self.initial_guess) != self.n:
+			raise ValueError("Initial guess must have same dimension as input function")
