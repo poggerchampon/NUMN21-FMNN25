4ebffc462eeacc49d9d7b4873fdb8ee998bbf83e
byorund <144137564+byorund@users.noreply.github.com>
2023-09-26 07:36:36 +0200

    Update dfp.py

--- Project 2 - Optimisation/src/optimization_methods/dfp.py
+++ Project 2 - Optimisation/src/optimization_methods/dfp.py
@@ -1,26 +1,24 @@
-
-from numpy import *
-from matplotlib.pyplot import *
+import numpy as np
 from .newton_inexact_line_search import NewtonInexactLineSearch 
 
 class DFP(NewtonInexactLineSearch):
     
     def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=1000, initial_guess = None):
     
-        super().__init__(self, opt_problem, n, h, tolerance, max_iterations, initial_guess)
-                    # Start with zeros if no initial guess is specified
-        x = self.initial_guess if self.initial_guess is not None else zeros(self.n)
-        
-        self.H = inv_approximate_hessian(self.opt_problem.objective_func, \
-                                       self.opt_problem.gradient_func, x)
+        super().__init__(opt_problem, n, h, tolerance, max_iterations, initial_guess)
+        # Start with zeros if no initial guess is specified
+        x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
+        self.H = inv_approximate_hessian(self.opt_problem.objective_func, self.opt_problem.gradient_func, x)
     
     def compute_direction(self, x, evaluate_func, gradient_func, current_gradient):
-        
+        self.update_hessian_invHessian()
         s = -self.H @ current_gradient
-        
         return s
         
     def update_hessian_invHessian(self):
+        # won't have 2 old points in the beginning
+        if len(self.path) < 2:
+            return
         
         x_new = self.path[-1]
         x_old = self.path[-2]
@@ -34,6 +32,5 @@ class DFP(NewtonInexactLineSearch):
         gamma = grad_new - grad_old
         
         # Updating H
-        
         self.H = H + (delta @ delta.T)*(1/(delta.T @ gamma)) \
             - (H @ gamma @ gamma.T @ H)*(1/(gamma.T @ H @ gamma))
