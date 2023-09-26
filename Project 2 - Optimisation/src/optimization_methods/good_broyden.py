ba2fb1f8fb71599e5f1744039953359553a94258
byorund <144137564+byorund@users.noreply.github.com>
2023-09-26 07:39:05 +0200

    Update good_broyden.py

--- Project 2 - Optimisation/src/optimization_methods/good_broyden.py
+++ Project 2 - Optimisation/src/optimization_methods/good_broyden.py
@@ -1,28 +1,26 @@
-from numpy import *
-from matplotlib.pyplot import *
+import numpy as np
 from .newton_inexact_line_search import NewtonInexactLineSearch 
-
 
 class GoodBroyden(NewtonInexactLineSearch):
     
     def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=1000, initial_guess = None):
     
-        super().__init__(self, opt_problem, n, h, tolerance, max_iterations, initial_guess)
-                    # Start with zeros if no initial guess is specified
-        x = self.initial_guess if self.initial_guess is not None else zeros(self.n)
+        super().__init__(opt_problem, n, h, tolerance, max_iterations, initial_guess)
+        # Start with zeros if no initial guess is specified
+        x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
         
-        self.Q = approximate_hessian(self.opt_problem.objective_func, \ 
-                                     self.opt_problem.gradient_func, x = None)  
-        self.H = inv_approximate_hessian(self.opt_problem.objective_func, \
-                                       self.opt_problem.gradient_func, x = None)
+        self.Q = approximate_hessian(self.opt_problem.objective_func, self.opt_problem.gradient_func, x)  
+        self.H = inv_approximate_hessian(self.opt_problem.objective_func, self.opt_problem.gradient_func, x)
     
     def compute_direction(self, x, evaluate_func, gradient_func, current_gradient):
-        
+        self.update_hessian_invHessian()
         s = -self.H @ current_gradient
-        
         return s
         
     def update_hessian_invHessian(self):
+        # won't have 2 old points in the beginning
+        if len(self.path) < 2:
+            return
         
         x_new = self.path[-1]
         x_old = self.path[-2]
@@ -39,11 +37,7 @@ class GoodBroyden(NewtonInexactLineSearch):
         v = (gamma - Q @ delta)*(1/(delta.T @ delta))
         
         # Updating H
-        
         self.H = H + (1/(delta.T @ H @ gamma))*(delta - H @ gamma) @ (delta.T @ H) 
         
         # Update Q
-        
         self.Q = Q + v @ delta.T
-        
-    
