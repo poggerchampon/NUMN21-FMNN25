b861db09f4f801d2405f6858eb493b12d2df5305
byorund <144137564+byorund@users.noreply.github.com>
2023-09-26 07:29:02 +0200

    Update __init__.py

--- Project 2 - Optimisation/src/optimization_methods/__init__.py
+++ Project 2 - Optimisation/src/optimization_methods/__init__.py
@@ -4,4 +4,21 @@ from .classical_newton_method import ClassicalNewtonMethod
 from .newton_exact_line_search import NewtonExactLineSearch
 from .newton_inexact_line_search import NewtonInexactLineSearch
 
-__all__ = ['OptimizationMethod', 'ClassicalNewtonMethod', 'NewtonExactLineSearch', 'NewtonInexactLineSearch']
+# QuasiNewton
+from .good_broyden import GoodBroyden
+from .bad_broyden import BadBroydenn
+from .symmetric_broyden import SymmetricBroyden
+from .dfp import DFP
+from .BFGS import BFGS
+
+__all__ = [
+    'OptimizationMethod', 
+    'ClassicalNewtonMethod', 
+    'NewtonExactLineSearch', 
+    'NewtonInexactLineSearch', 
+    'GoodBroyden', 
+    'BadBroyden', 
+    'SymmetricBroyden', 
+    'DFP', 
+    'BFGS'
+]
