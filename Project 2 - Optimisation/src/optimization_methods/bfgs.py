ab4e07e5bbbb88b6cbbc2ccb67a3bfd800e5720e
byorund <144137564+byorund@users.noreply.github.com>
2023-09-29 19:13:03 +0200

    Update bfgs.py

--- Project 2 - Optimisation/src/optimization_methods/bfgs.py
+++ Project 2 - Optimisation/src/optimization_methods/bfgs.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-from .newton_exact_line_search import NewtonExactLineSearch 
+from .newton_inexact_line_search import NewtonInexactLineSearch 
 from src.functions import inv_approximate_hessian
 
-class BFGS(NewtonExactLineSearch):
+class BFGS(NewtonInexactLineSearch):
     
-    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=1000, initial_guess = None):
+    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-2, max_iterations=1000, initial_guess = None):
         super().__init__(opt_problem, n, h, tolerance, max_iterations, initial_guess)
         # Start with zeros if no initial guess is specified
         x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
-        self.H_inv = inv_approximate_hessian(self.opt_problem.gradient_func, x, n)
+        self.H_inv = inv_approximate_hessian(self.opt_problem.get_gradient(), x, n)
     
     def compute_direction(self, x, gradient_func, current_gradient):
         return -np.dot(self.H_inv, current_gradient)
@@ -23,13 +23,13 @@ class BFGS(NewtonExactLineSearch):
             H_inv_g = np.dot(self.H_inv, delta_g)
             g_H_inv = np.dot(delta_g.T, self.H_inv)
             
-            first_term_1 = 1 + (np.dot(delta_g, H_inv_g) / np.dot(delta_x.T, delta_g))
+            first_term_1 = 1 + (np.dot(delta_g.T, H_inv_g) / np.dot(delta_x.T, delta_g))
             first_term_2 = np.dot(delta_x, delta_x.T) / np.dot(delta_x.T, delta_g)
-            first_term = np.dot(first_term_1, first_term_2)
+            first_term = first_term_1 * first_term_2
             
             second_term_numerator = np.dot(delta_x, g_H_inv) + np.dot(H_inv_g, delta_x.T)
             second_term_denominator = np.dot(delta_x.T, delta_g)
             second_term = second_term_numerator / second_term_denominator
             
-            
             self.H_inv += (first_term - second_term)
+            
