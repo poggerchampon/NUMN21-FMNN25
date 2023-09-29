bfad91555cd1756f6bc9463e763a16d2c8217c05
byorund <144137564+byorund@users.noreply.github.com>
2023-09-29 18:33:57 +0200

    Update bad_broyden.py

--- Project 2 - Optimisation/src/optimization_methods/bad_broyden.py
+++ Project 2 - Optimisation/src/optimization_methods/bad_broyden.py
@@ -1,38 +1,34 @@
 import numpy as np
 
-from .newton_inexact_line_search import NewtonInexactLineSearch
+from .newton_exact_line_search import NewtonExactLineSearch
 from src.functions import inv_approximate_hessian
 
-class BadBroyden(NewtonInexactLineSearch):
-    
-    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=1000, initial_guess = None):
+class BadBroyden(NewtonExactLineSearch):
+    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-2, max_iterations=1000, initial_guess = None):
         super().__init__(opt_problem, n, h, tolerance, max_iterations, initial_guess)
         
         # Start with zeros if no initial guess is specified
         x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
-        self.H = inv_approximate_hessian(self.opt_problem.gradient_func, x, n)
+        self.H_inv = inv_approximate_hessian(self.opt_problem.get_gradient(), x, n)
     
     def compute_direction(self, x, gradient_func, current_gradient):
-        s = -self.H @ current_gradient
-        return s
+        return -np.dot(self.H_inv, current_gradient)
         
-    def update_inv_hessian(self):
-        # won't have 2 old points in the beginning
-        if len(self.path) < 2:
-            return
+    def update_inv_hessian(self, x, gradient_func, current_gradient):
+        if self.prev_x is not None and self.prev_gradient is not None:
+            delta_x = x - self.prev_x 
+            delta_g = current_gradient - self.prev_gradient
         
-        x_new = self.path[-1]
-        x_old = self.path[-2]
+            B_inv_gamma = np.dot(self.H_inv, delta_g)
+            numerator = np.dot((delta_x - B_inv_gamma), delta_g.T)
+            denominator = np.dot(delta_g.T, delta_g)
         
-        grad_new = self.opt_problem.gradient(x_new)
-        grad_old = self.opt_problem.gradient(x_old)
+            if denominator != 0:
+                self.H_inv += numerator / denominator
+            else:
+                print("Division by zero encountered in update_inv_hessian, skipping this update")
         
-        H = self.H    
-        
-        delta = x_new - x_old 
-        gamma = grad_new - grad_old
-        
-        # Updating H
-        
-        self.H = H + (1/(gamma.T @ gamma))*(delta - H @ gamma) @ gamma.T 
-        
+    def solve(self):
+        self.prev_x = None
+        self.prev_gradient = None
+        return super().solve()
