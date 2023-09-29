4fb491072c49885aab89e32546b41c970155e199
byorund <144137564+byorund@users.noreply.github.com>
2023-09-29 18:42:38 +0200

    Update symmetric_broyden.py

--- Project 2 - Optimisation/src/optimization_methods/symmetric_broyden.py
+++ Project 2 - Optimisation/src/optimization_methods/symmetric_broyden.py
@@ -1,37 +1,26 @@
 import numpy as np
 
-from .newton_inexact_line_search import NewtonInexactLineSearch 
+from .newton_exact_line_search import NewtonExactLineSearch 
 from src.functions import inv_approximate_hessian
 
-class SymmetricBroyden(NewtonInexactLineSearch):
+class SymmetricBroyden(NewtonExactLineSearch):
     
-    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=1000, initial_guess = None):
+    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-2, max_iterations=1500, initial_guess = None):
         super().__init__(opt_problem, n, h, tolerance, max_iterations, initial_guess)
         # Start with zeros if no initial guess is specified
         x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
-        self.H = inv_approximate_hessian(self.opt_problem.gradient_func, x, n)
+        self.H_inv = inv_approximate_hessian(self.opt_problem.get_gradient(), x, n)
     
     def compute_direction(self, x, gradient_func, current_gradient):
-        return -self.H @ current_gradient
+        return -np.dot(self.H_inv, current_gradient)
         
-    def update_inv_hessian(self):
-        # won't have 2 old points in the beginning
-        if len(self.path) < 2:
-            return
-        
-        x_new = self.path[-1]
-        x_old = self.path[-2]
-        
-        grad_new = self.opt_problem.gradient(x_new)
-        grad_old = self.opt_problem.gradient(x_old)
-        
-        H = self.H    
-        
-        delta = x_new - x_old 
-        gamma = grad_new - grad_old
-        
-        u = delta - H @ gamma
-        a = 1/(u.T @ gamma)
-        
-        # Updating H
-        self.H = H + a*u @ u.T
+    def update_inv_hessian(self, x, gradient_func, current_gradient):
+        if self.prev_x is not None and self.prev_gradient is not None:
+            delta_x = x - self.prev_x 
+            delta_g = current_gradient - self.prev_gradient
+            
+            u = delta_x - np.dot(self.H_inv, delta_g)
+            a = 1/(np.dot(u.T, delta_g))
+            
+            # Updating H
+            self.H_inv += a * np.dot(u, u.T)
