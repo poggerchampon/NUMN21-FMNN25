b8984724b58bdff141e2ccf23d8677a0cabe3bdb
byorund <144137564+byorund@users.noreply.github.com>
2023-09-29 18:48:15 +0200

    Update dfp.py

--- Project 2 - Optimisation/src/optimization_methods/dfp.py
+++ Project 2 - Optimisation/src/optimization_methods/dfp.py
@@ -5,32 +5,24 @@ from src.functions import inv_approximate_hessian
 
 class DFP(NewtonInexactLineSearch):
     
-    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=1000, initial_guess = None):
-    
+    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=2000, initial_guess = None):
         super().__init__(opt_problem, n, h, tolerance, max_iterations, initial_guess)
         # Start with zeros if no initial guess is specified
         x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
-        self.H = inv_approximate_hessian(self.opt_problem.gradient_func, x, n)
+        self.H_inv = inv_approximate_hessian(self.opt_problem.get_gradient(), x, n)
     
     def compute_direction(self, x, gradient_func, current_gradient):
-        return -self.H @ current_gradient
+        return -np.dot(self.H_inv, current_gradient)
         
-    def update_inv_hessian(self):
-        # won't have 2 old points in the beginning
-        if len(self.path) < 2:
-            return
-        
-        x_new = self.path[-1]
-        x_old = self.path[-2]
-        
-        grad_new = self.opt_problem.gradient(x_new)
-        grad_old = self.opt_problem.gradient(x_old)
-        
-        H = self.H    
-        
-        delta = x_new - x_old 
-        gamma = grad_new - grad_old
-        
-        # Updating H
-        self.H = H + (delta @ delta.T)*(1/(delta.T @ gamma)) \
-            - (H @ gamma @ gamma.T @ H)*(1/(gamma.T @ H @ gamma))
+    def update_inv_hessian(self, x, gradient_func, current_gradient):
+        if self.prev_x is not None and self.prev_gradient is not None:
+            delta_x = (x - self.prev_x).reshape(-1, 1) 
+            delta_g = (current_gradient - self.prev_gradient).reshape(-1, 1) 
+            
+            first_term = np.dot(delta_x, delta_x.T) / np.dot(delta_x.T, delta_g)
+            
+            H_inv_g = np.dot(self.H_inv, delta_g)
+            g_H_inv = np.dot(delta_g.T, self.H_inv)
+            second_term = np.dot(H_inv_g, g_H_inv) / np.dot(delta_g.T, H_inv_g)
+            
+            self.H_inv += (first_term - second_term)
