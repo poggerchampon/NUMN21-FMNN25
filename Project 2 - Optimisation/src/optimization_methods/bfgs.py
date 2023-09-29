3dfcbf7ce07bd256e3cf59ed452bc9cffb52ef45
byorund <144137564+byorund@users.noreply.github.com>
2023-09-29 18:55:42 +0200

    Update bfgs.py

--- Project 2 - Optimisation/src/optimization_methods/bfgs.py
+++ Project 2 - Optimisation/src/optimization_methods/bfgs.py
@@ -1,36 +1,35 @@
 import numpy as np
 
-from .newton_inexact_line_search import NewtonInexactLineSearch 
+from .newton_exact_line_search import NewtonExactLineSearch 
 from src.functions import inv_approximate_hessian
 
-class BFGS(NewtonInexactLineSearch):
+class BFGS(NewtonExactLineSearch):
     
     def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=1000, initial_guess = None):
-    
         super().__init__(opt_problem, n, h, tolerance, max_iterations, initial_guess)
         # Start with zeros if no initial guess is specified
         x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
-        self.H = inv_approximate_hessian(self.opt_problem.gradient_func, x, n)
+        self.H_inv = inv_approximate_hessian(self.opt_problem.gradient_func, x, n)
     
     def compute_direction(self, x, gradient_func, current_gradient):
-        return -self.H @ current_gradient
+        return -np.dot(self.H_inv, current_gradient)
         
-    def update_inv_hessian(self):
-        # won't have 2 old points in the beginning
-        if len(self.path) < 2:
-            return
-        
-        x_new = self.path[-1]
-        x_old = self.path[-2]
-        
-        grad_new = self.opt_problem.gradient(x_new)
-        grad_old = self.opt_problem.gradient(x_old)
-        
-        H = self.H    
-        
-        delta = x_new - x_old 
-        gamma = grad_new - grad_old
-        
-        # Updating H
-        self.H = H + (1 + (gamma.T @ H @ gamma)/(delta.T @ gamma))*((delta @ delta.T)*(1/(delta.T @ gamma))) \
-            - (delta @ gamma.T @ H + H @ gamma @ delta.T)*(1/(delta.T @ gamma))
+    def update_inv_hessian(self, x, gradient_func, current_gradient):
+        if self.prev_x is not None and self.prev_gradient is not None:
+            delta_x = (x - self.prev_x).reshape(-1, 1) 
+            delta_g = (current_gradient - self.prev_gradient).reshape(-1, 1) 
+            
+            # ik this is unreadable, but so is the math
+            H_inv_g = np.dot(self.H_inv, delta_g)
+            g_H_inv = np.dot(delta_g.T, self.H_inv)
+            
+            first_term_1 = 1 + (np.dot(delta_g, H_inv_g) / np.dot(delta_x.T, delta_g))
+            first_term_2 = np.dot(delta_x, delta_x.T) / np.dot(delta_x.T, delta_g)
+            first_term = np.dot(first_term_1, first_term_2)
+            
+            second_term_numerator = np.dot(delta_x, g_H_inv) + np.dot(H_inv_g, delta_x.T)
+            second_term_denominator = np.dot(delta_x.T, delta_g)
+            second_term = second_term_numerator / second_term_denominator
+            
+            
+            self.H_inv += (first_term - second_term)
