3e250b140c3bb64e40a59b1f25fa5eb9a6254998
byorund <144137564+byorund@users.noreply.github.com>
2023-10-01 17:07:52 +0200

    Update good_broyden.py

--- Project 2 - Optimisation/src/optimization_methods/good_broyden.py
+++ Project 2 - Optimisation/src/optimization_methods/good_broyden.py
@@ -5,16 +5,15 @@ from src.functions import inv_approximate_hessian
 
 class GoodBroyden(NewtonInexactLineSearch):
     
-    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-3, max_iterations=1000, initial_guess = None):
-        super().__init__(opt_problem, n, h, tolerance, max_iterations, initial_guess)
+    def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-3, max_iterations=1000, initial_guess=None, progress_bar=False):
+        super().__init__(opt_problem, n, h, tolerance, max_iterations, initial_guess, progress_bar)
 
-        x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
-        self.H_inv = inv_approximate_hessian(self.opt_problem.get_gradient(), x, n)
+        self.H_inv = np.eye(n) # Initialise inverse hessian (identity matrix)
         
-    def compute_direction(self, x, gradient_func, current_gradient):
+    def _compute_direction(self, x, gradient_func, current_gradient):
         return -np.dot(self.H_inv, current_gradient)
         
-    def update_inv_hessian(self, x, gradient_func, current_gradient):
+    def _update_inv_hessian(self, x, gradient_func, current_gradient):
         if self.prev_x is not None and self.prev_gradient is not None:
             delta_x = x - self.prev_x
             delta_g = current_gradient - self.prev_gradient
