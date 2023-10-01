c14a08444f0c3ad720f632f780258ce2951e4b66
byorund <144137564+byorund@users.noreply.github.com>
2023-10-01 17:06:21 +0200

    Update classical_newton_method.py

--- Project 2 - Optimisation/src/optimization_methods/classical_newton_method.py
+++ Project 2 - Optimisation/src/optimization_methods/classical_newton_method.py
@@ -5,13 +5,14 @@ from tqdm import tqdm
 import numpy as np
 
 class ClassicalNewtonMethod(OptimizationMethod):
-	def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=1000, initial_guess=None):
+	def __init__(self, opt_problem, n, h=1e-5, tolerance=1e-5, max_iterations=1000, initial_guess=None, progress_bar=False):
 		super().__init__(opt_problem)
 		self.n = n
 		self.h = h
 		self.tolerance = tolerance
 		self.max_iterations = max_iterations
 		self.initial_guess = initial_guess
+		self.progress_bar = progress_bar
 		
 		# Store previous x and gradient
 		self.prev_x = None
@@ -23,21 +24,20 @@ class ClassicalNewtonMethod(OptimizationMethod):
 		
 	# Method for finding step size alpha. 
 	# Is overridden in derived line search methods
-	def compute_alpha(self, x, direction):
-		# In classical Newton, alpha is always 1
-		return 1.0
+	def _compute_alpha(self, x, direction):
+		return 1.0 # In classical Newton, alpha is always 1
 	
 	# Method for finding the direction. Default here is classical newton
-	# Can be overridden in derived classes like DFP, SimpleBroyden, BFGS etc
-	def compute_direction(self, x, gradient_func, current_gradient):
-		H = approximate_hessian(gradient_func, x, self.n)
+	# Is overridden in derived Quasi Newton classes
+	def _compute_direction(self, x, gradient_func, current_gradient):
+		H = approximate_hessian(self.opt_problem.get_evaluate(), gradient_func, x, self.n)
 		G = 0.5 * (H + H.T)  # Make symmetric if necessary
 		
 		direction = -np.linalg.solve(G, current_gradient)  # Newton's direction
 		return direction
 		
-	# hessian update method for Quasi newton methods
-	def update_inv_hessian(self, x, gradient_func, current_gradient):
+	# Hessian update method for Quasi newton methods
+	def _update_inv_hessian(self, x, gradient_func, current_gradient):
 		pass
 	
 	def solve(self):	
@@ -45,46 +45,48 @@ class ClassicalNewtonMethod(OptimizationMethod):
 		
 		# Start with zeros if no initial guess is specified
 		x = self.initial_guess if self.initial_guess is not None else np.zeros(self.n)
-		iteration = 0
 		
-		# Use tqdm for neat progress bar
-		with tqdm(total=self.max_iterations, desc="Optimizing", unit="iteration") as pbar:
-			while True:
-				current_gradient = gradient_func(x)
-				
+		# Conditionally use tqdm for progress bar
+		pbar = tqdm(total=self.max_iterations, desc="Optimizing", unit="iteration") if self.progress_bar else None
+		iteration = 0
+		while True:
+			current_gradient = gradient_func(x)
+			
+			if self.progress_bar:
 				x_str = np.array2string(x, formatter={'all': lambda x: f'{x:.2e}'})
 				pbar.set_description(f"Optimizing (x = {x_str})")
 				
-				# Stopping criteria
-				if np.linalg.norm(current_gradient) < self.tolerance or iteration >= self.max_iterations:
-					return x
+			# Stopping criteria
+			if np.linalg.norm(current_gradient) < self.tolerance or iteration >= self.max_iterations:
+				if self.progress_bar:
+					pbar.close()
+				return x
 				
-				# Get direction and step size using either classical method or derived method 
-				direction = self.compute_direction(x, gradient_func, current_gradient)
-				alpha = self.compute_alpha(x, direction)
+			# Get direction and step size using either classical method or derived method. Then update hessian
+			direction = self._compute_direction(x, gradient_func, current_gradient)
+			alpha = self._compute_alpha(x, direction)
+			self._update_inv_hessian(x, gradient_func, current_gradient)
 				
-				# update hessian
-				self.update_inv_hessian(x, gradient_func, current_gradient)
+			self.prev_x = x.copy()
+			self.prev_gradient = current_gradient
 				
-				self.prev_x = x.copy()
-				self.prev_gradient = current_gradient
+			# Update the current point and save it
+			x += alpha * direction
+			self.path.append(x.copy())
 				
-				# Update the current point and save it
-				x += alpha * direction
-				self.path.append(x.copy())
-				
-				# Update the progress bar
+			# Update the progress bar
+			if self.progress_bar:
 				pbar.update(1)
-				iteration += 1
+			iteration += 1
 				
-			# Minimum found
-			return x
+		# Minimum found
+		return x
 	
 	def validate_params(self):
 		# Check that parameters are greater than zero
 		if self.h <= 0 or self.tolerance <= 0 or self.max_iterations <= 0 or self.n <= 0:
 			raise ValueError("Parameters should be greater than zero")
-				
+		
+		# Check matching dimensions
 		if self.initial_guess is not None and len(self.initial_guess) != self.n:
 			raise ValueError("Initial guess must have same dimension as input function")
-			
