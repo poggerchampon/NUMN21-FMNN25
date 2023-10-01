650ddd8f4f5e0394b756da7b428c44d438e4e9b5
byorund <144137564+byorund@users.noreply.github.com>
2023-10-01 17:09:44 +0200

    Update helper_methods.py

--- Project 2 - Optimisation/src/functions/helper_methods.py
+++ Project 2 - Optimisation/src/functions/helper_methods.py
@@ -8,33 +8,39 @@ def numerical_gradient(evaluate_func, x, h=1e-5):
     for i in range(len(x)):
         grad[i] = (evaluate_func(x + h * identity_matrix[i]) - evaluate_func(x - h * identity_matrix[i])) / (2.0 * h)
     
-    return np.array(grad)
+    return grad
 
 # Returns the inverse of hessian
-def inv_approximate_hessian(gradient_func, x, n, h=1e-5):
-    return np.linalg.inv(approximate_hessian(gradient_func, x, n, h))
+def inv_approximate_hessian(evaluate_func, gradient_func, x, n, h=1e-5):
+    return np.linalg.inv(approximate_hessian(evaluate_func, gradient_func, x, n, h))
 
-# numerical_hessian() misses off-diagonal elements, also misses last row because of
-# 'dim-1' in the loop
-def approximate_hessian(gradient_func, x, n, h=1e-5):
+def approximate_hessian(evaluate_func, gradient_func, x, n, h=1e-5):
     hessian = np.zeros((n, n))
     identity_matrix = np.eye(n)
-    base_gradient = gradient_func(x)
     
     for i in range(n):
         # Diagonal elements
         x_i_pos = x + h * identity_matrix[i]
-        gradient_i_pos = gradient_func(x_i_pos)
-        hessian[i][i] = (gradient_i_pos[i] - base_gradient[i]) / h
+        x_i_neg = x - h * identity_matrix[i]
+        f_i_pos = evaluate_func(x_i_pos)
+        f_i_neg = evaluate_func(x_i_neg)
+        hessian[i][i] = (f_i_pos - 2 * evaluate_func(x) + f_i_neg) / (h**2)
         
         # Start from i + 1 to avoid recomputing diagonal, and use symmetry
         for j in range(i+1, n):
-            x_j_pos = x + h * identity_matrix[j]
-            gradient_j_pos = gradient_func(x_j_pos)
+            x_ij_pos = x + h * (identity_matrix[i] + identity_matrix[j])
+            x_ij_neg = x - h * (identity_matrix[i] + identity_matrix[j])
+            x_i_pos_j_neg = x + h * identity_matrix[i] - h * identity_matrix[j]
+            x_i_neg_j_pos = x - h * identity_matrix[i] + h * identity_matrix[j]
             
-            # Take advantage of symmetry to compute both sides of the matrix
-            value = (gradient_j_pos[i] - base_gradient[i]) / h
-            hessian[i][j] = value
-            hessian[j][i] = value
+            f_ij_pos = evaluate_func(x_ij_pos)
+            f_ij_neg = evaluate_func(x_ij_neg)
+            f_i_pos_j_neg = evaluate_func(x_i_pos_j_neg)
+            f_i_neg_j_pos = evaluate_func(x_i_neg_j_pos)
+            
+            hessian[i][j] = (f_ij_pos - f_i_pos_j_neg - f_i_neg_j_pos + f_ij_neg) / (4 * h**2)
+            hessian[j][i] = hessian[i][j] 
             
     return hessian
+
+
