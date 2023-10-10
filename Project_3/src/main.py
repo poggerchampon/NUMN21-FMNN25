2668f67147399f4e979302368dd0d6212c852f57
byorund <144137564+byorund@users.noreply.github.com>
2023-10-10 17:01:03 +0200

    Update main.py

--- Project_3/src/main.py
+++ Project_3/src/main.py
@@ -1,64 +1,28 @@
 from mpi4py import MPI
 import numpy as np
 
-from src.utilities.constants import omega, dx, num_iter, u_normal, u_heater, u_window
-from src.solver.laplace_solver import solve_laplace
+from src.solver.dirichlet_neumann_solver import DirichletNeumannSolver
 from src.utilities.plot_temperature import plot_temperature
 from models.load_apartment import load_apartment_layout
+from models.room import Room
 		
-def dirichlet_neumann_iteration(u, comm, rank):
-	# Dirichlet-Neumann Iteration
-	for k in range(num_iter):
-		
-		if rank == 1:
-			# Send Dirichlet conditions to adjacent rooms
-			comm.send(u[:, 0], dest=0, tag=11)
-			comm.send(u[:, -1], dest=2, tag=12)
-			
-		elif rank == 0:
-			# Receive Dirichlet conditions from room 2
-			u_k1_2 = comm.recv(source=1, tag=11)
-			
-			# Update right wall of room 1 with Dirichlet conditions from room 2
-			u[:, -1] = u_k1_2[0]
-			
-			# Calculate flux for Neumann condition
-			flux = (u[:, -2] - u[:, -1]) * dx
-			u[:, -1] = u[:, -2] - dx * flux
-			
-		else:
-			# Receive Dirichlet conditions from room 2
-			u_k1_2 = comm.recv(source=1, tag=12)
-			
-			# Update left wall of room 3 with Dirichlet conditions from room 2
-			u[:, 0] = u_k1_2[0]
-			
-			# Calculate flux for Neumann condition
-			flux = (u[:, 1] - u[:, 0]) * dx
-			u[:, 0] = u[:, 1] - dx * flux
-			
-		u_new = solve_laplace(u)
-		u_new = omega * u_new + (1 - omega) * u
-		
-	return u_new
-
-def output_results(u, rank):
+def output_results(u_dic, rank):
 	room_number = rank + 1
-	plot_temperature(u, room_number)
+	plot_temperature(u_dic[rank], room_number)
 
 def main():
 	# Initialise MPI
 	comm = MPI.COMM_WORLD
 	rank = comm.Get_rank()
 	
-	# Load apartment layout and set the boundary conditions
+	# Load apartment layout with conditions
 	rooms = load_apartment_layout(filename="models/apartment.json")
-	for room in rooms:
-		room.set_boundary_conditions()
 	
-	u = rooms[rank].temperature
-	u_new = dirichlet_neumann_iteration(u, comm, rank)
+	# Solve using DirichletNeumann method
+	solver = DirichletNeumannSolver(rooms, comm, rank)
+	u_new = solver.solve()
 	
+	# Plot temperature distribution
 	output_results(u_new, rank)
 		
 	# Terminate MPI
