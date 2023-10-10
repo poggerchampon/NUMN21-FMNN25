f0ed3f922abe421b899f1158da2f6ece50c931e8
byorund <144137564+byorund@users.noreply.github.com>
2023-10-10 16:02:37 +0200

    Update load_apartment.py

--- Project_3/src/models/load_apartment.py
+++ Project_3/src/models/load_apartment.py
@@ -1,9 +1,10 @@
-from room import Room
 import numpy as np
 import json
 
-def load_apartment_layout(filename='apartment.json'):
-	with open('apartment.json', 'r') as f:
+from .room import Room
+
+def load_apartment_layout(filename='models/apartment.json'):
+	with open(filename, 'r') as f:
 		apart_config = json.load(f)
 	
 	rooms = [Room(**room_config) for room_config in apart_config['rooms']]
