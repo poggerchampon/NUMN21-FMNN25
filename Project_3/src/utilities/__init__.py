e1d34dbe06cdeb572f6f55e6490ab6e3d72a0227
byorund <144137564+byorund@users.noreply.github.com>
2023-10-11 09:58:37 +0200

    Update __init__.py

--- Project_3/src/utilities/__init__.py
+++ Project_3/src/utilities/__init__.py
@@ -1,6 +1,8 @@
+from .help_functions import interpolate_boundary
 from .plot_temperature import plot_temperature
 from . import constants
 
 __all__ = [
+	"interpolate_boundary",
 	"plot_temperature"
 ]
