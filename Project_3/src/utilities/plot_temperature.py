1194f2c830fb33d20f06edc311c0204ecfaa73d3
byorund <144137564+byorund@users.noreply.github.com>
2023-10-12 09:04:40 +0200

    Update plot_temperature.py

--- Project_3/src/utilities/plot_temperature.py
+++ Project_3/src/utilities/plot_temperature.py
@@ -2,8 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_temperature(u, room_number, min_temperature=0, max_temperature=40):
-    # Ignore the edges, as they are boundary conditions
-    u_plot = u[1:-1, 1:-1]
+    """
+    Plots the temperature distribution for a given room based on a 2D numpy array of temperatures.
+
+    Parameters:
+    - u : A 2D numpy array representing the temperature at each grid point. The edges of this array are treated as boundary conditions and are not plotted.
+    - room_number: The identifier for the room, used in the plot title.
+    - min_temperature (optional): The minimum temperature value for color scaling. Defaults to 0.
+    - max_temperature (optional): The maximum temperature value for color scaling. Defaults to 40.
+    """
+    
+    u_plot = u[1:-1, 1:-1] # Ignore the edges, as they are boundary conditions
     
     plt.figure(figsize=(8, 8))
     plt.imshow(u_plot, cmap='hot', interpolation='nearest', vmin=min_temperature, vmax=max_temperature)
